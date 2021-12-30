30e744796ec3d72f1c21061831a780e3356bf4e5
Dan Ficklin <danficklin@fakemail.com>
2021-12-30 15:39:51 -0500

    Small updates, some things still not working

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ const baseURL = 'https://pokeapi.co/api/v2';
 
 let formElement = document.getElementById('search-form');
 let listElement = document.getElementById('pokemon-list');
-let totalPokemon = 151; 
+let totalPokemon = 898; 
 
 function randomPokemon() {
     return Math.floor((Math.random() * totalPokemon) +1);
@@ -13,10 +13,6 @@ function randomPokemon() {
 
 function fetchPokemonList() {
 
-// pokemon/1 returns the first Pokemon, pokemon/2 returns the second etc., 
-// so if I can get a random number in there somehow I can randomize which appears.
-
-//
 
     fetch(`${ baseURL }/pokemon/${ randomPokemon() }`) 
         .then(response => response.json())
@@ -32,29 +28,30 @@ function displayPokemon(pokemonData) {
     let pokemonTitle = document.createElement('h2');
     let pokemonImage = document.createElement('img');
     let pokemonType1 = document.createElement('h3');
-    // let pokemonType2 = document.createElement('h2');
 
     pokemonTitle.innerText = `It's ${pokemonData.name}`;
     pokemonImage.src = pokemonData.sprites.front_default;
-    pokemonType1.innerText = `${pokemonData.name} is a ${pokemonData.types[0].type.name} type Pokemon.`;
-    // pokemonType2.innerText = pokemonData.types[1].type.name;
+    pokemonType1.innerText = `${pokemonData.name} is a Pokemon of the  ${pokemonData.types[0].type.name} type.`;
 
 
     pokemonCard.classList.add('pokemon-card');
     pokemonTitle.classList.add('pokemon-title');
     pokemonImage.classList.add('pokemon-image');
     pokemonType1.classList.add('pokemon-type1');
-    // pokemonType2.classList.add('pokemon-type2');
 
     pokemonCard.appendChild(pokemonTitle);
     pokemonCard.appendChild(pokemonImage);
     pokemonCard.appendChild(pokemonType1);
-    // pokemonCard.appendChild(pokemonType2);
     
     listElement.appendChild(pokemonCard);
 }
 
-// How can I add a string of text before the name or type ex. "It's 'pokemon.name' "
-// or "pokemon.name is a types.0.name and types.1.name type pokemon."
 
 fetchPokemonList();
+
+$(':radio[name="generation"]').change(function() {
+    var totalPokemon = $(this).filter(':checked').val();
+    window.location.reload();
+    });
+
+    
